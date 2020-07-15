ff4568225a9d59faacbc52aef7590e91aa0f46b0
Jack Humphrey <jackhump@users.noreply.github.com>
2020-07-15 17:00:33 -0400

    ungroup query mod

--- R/standardize.R
+++ R/standardize.R
@@ -262,7 +262,8 @@ standardize_subset <- function(locus,
       dplyr::slice(1) %>%
       dplyr::group_by(SNP) %>%
       dplyr::slice(1)
-
+    # dplyr doesn't like working with grouped tables downstream.
+    query_mod <- ungroup(query_mod)
     # Trim whitespaces
     ## Extra whitespace causes problems when you try to make space-delimited files
     printer("++ Removing extra whitespace", v=verbose)
