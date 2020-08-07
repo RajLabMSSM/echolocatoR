cafbc02b342af0c87af34e750d3095e8d077b6ca
bschilder <brian.schilder@mssm.edu>
2020-08-07 16:54:08 -0400

    Fix saveSparse printer

--- R/utils.R
+++ R/utils.R
@@ -1144,15 +1144,15 @@ determine_chrom_type <- function(chrom_type=NULL,
 #' @family LD
 #' @keywords internal
 saveSparse <- function(LD_matrix,
-                        LD_path,
-                        verbose=T){
+                       LD_path,
+                       verbose=T){
   # https://cmdlinetips.com/2019/05/introduction-to-sparse-matrices-in-r/
   LD_sparse <- Matrix::Matrix(as.matrix(LD_matrix), sparse = T)
   # printer("Dense size:")
   # print(object.size(LD_matrix),units="auto")
   # printer("Sparse size:")
   # print(object.size(LD_sparse),units="auto")
-  printer("LD:: Saving LD as sparse matrix ==>",output_path,v=verbose)
+  printer("LD:: Saving LD as sparse matrix ==>",LD_path,v=verbose)
   saveRDS(LD_sparse, LD_path)
 }
 
