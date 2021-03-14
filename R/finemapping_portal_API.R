20aca243018ad611b4667e166aa9de52d9dff632
Brian M. Schilder <34280215+bschilder@users.noreply.github.com>
2021-03-14 15:46:36 +0000

    Update website articles

--- R/finemapping_portal_API.R
+++ R/finemapping_portal_API.R
@@ -1,21 +1,96 @@
 
 
+GITHUB.portal_metadata <- function(verbose=T){
+  printer("Fetching echolocatoR Fine-mapping Portal study metadata.",v=verbose)
+  meta <- data.table::fread("https://github.com/RajLabMSSM/Fine_Mapping_Shiny/raw/master/www/metadata/study_metadata.csv.gz")
+  return(meta)
+}
+
+
+
+
+
+#' Search and download fine-mapping files
+#'
+#' Search the \href{https://github.com/RajLabMSSM/Fine_Mapping_Shiny}{echolocatoR Fine-mapping Portal}
+#' for fine-mapping results, LD, and locus plots.
+#' @examples
+#' local_finemap <- GITHUB.portal_query(dataset_types="GWAS", phenotypes = c("schizophrenia","parkinson"),file_types = "multi_finemap", LD_panels=c("UKB","1KGphase3"))
+GITHUB.portal_query <- function(dataset_types=NULL,
+                                datasets=NULL,
+                                phenotypes=NULL,
+                                loci=NULL,
+                                LD_panels=c("UKB","1KGphase1","1KGphase3"),
+                                file_types=c("multi_finemap","LD","plot"),
+                                results_dir="./",
+                                overwrite=F,
+                                nThread=parallel::detectCores()-2,
+                                verbose=T){
+  #### Search metadata ####
+  meta <- GITHUB.portal_metadata(verbose=verbose)
+  meta <- if(!is.null(dataset_types)) subset(meta, tolower(dataset_type) %in% tolower(dataset_types)) else meta
+  meta <- if(!is.null(datasets)) meta[grepl(paste(datasets,collapse = "|"),meta$dataset,ignore.case = T),] else meta
+  meta <- if(!is.null(phenotypes)) meta[grepl(paste(phenotypes,collapse = "|"),meta$phenotype,ignore.case = T),] else meta
+  printer("+",nrow(meta),"datasets remain after filtering.",v=verbose)
+
+  #### Find URLs ####
+  file_type_dict <- c("multi_finemap"=".multi_finemap.csv.gz",
+                      "LD"=".LD.csv.gz",
+                      "plot"=".png")
+  file_urls <- lapply(file_types, function(ftype){
+   printer("+ Searching for",ftype,"files...",v=verbose)
+   remote_finemap <- GITHUB.list_files(creator="RajLabMSSM",
+                                       repo="Fine_Mapping_Shiny",
+                                       query=file_type_dict[[ftype]],
+                                       # query= paste(paste0("(",paste(unique(meta$dataset_type), collapse = "|"),")"),
+                                       #              paste0("(",file_type_dict[[ftype]],")"),sep="&&"),
+                                       verbose = F)
+   return(data.table::data.table(URL=remote_finemap, file_type=ftype))
+ }) %>% data.table::rbindlist() %>%
+   # make sure to remove anything that's not in the data folder (e.g. icons)
+   subset(startsWith(URL,"https://github.com/RajLabMSSM/Fine_Mapping_Shiny/raw/master/www/data"))
+
+  #### Format url results ####
+  file_filt <- file_urls %>%
+   dplyr::mutate(url_strip=gsub("https://github.com/RajLabMSSM/Fine_Mapping_Shiny/raw/master/www/data/","",URL)) %>%
+   tidyr::separate(col = "url_strip",sep = "/", into = c("dataset_type","dataset","locus"), remove = F, extra = "drop") %>%
+   subset(dataset_type %in% unique(meta$dataset_type) &
+          dataset %in% unique(meta$dataset))
+
+ ## Let's just download the loci of interest.
+ if(!is.null(loci)) file_filt <- subset(file_filt, locus %in% loci)
+ printer("+",nrow(file_filt),"unique files identified.",v=verbose)
+ ## Filter by LD panel
+ file_filt <- file_filt[grepl(paste(LD_panels,collapse = "|"),file_filt$URL),]
+
+  ### Download files ####
+  local_finemap <- GITHUB.download_files(filelist = unique(file_filt$URL),
+                                         download_dir = results_dir,
+                                         overwrite = overwrite,
+                                         nThread = nThread,
+                                         verbose = verbose)
+ printer("+ Returning local file paths.",v=verbose)
+ return(local_finemap)
+}
+
+
 
 GITHUB.list_files <- function(creator="RajLabMSSM",
                               repo="Fine_Mapping_Shiny",
                               query=NULL,
-                              return_download_api=T){
+                              return_download_api=T,
+                              verbose=T){
     repo_api <- file.path("https://api.github.com/repos",creator,repo,
                           "git/trees/master?recursive=1")
   req <- httr::GET(repo_api)
   httr::stop_for_status(req)
   filelist <- unlist(lapply(httr::content(req)$tree, "[", "path"), use.names = F)
-  print(paste(length(filelist),"files found in GitHub repo:", file.path(creator,repo)))
+  printer(paste(length(filelist),"files found in GitHub repo:", file.path(creator,repo)),v=verbose)
   if(!is.null(query)){
     # query_string <- "*Nalls23andMe_2019.*UKB.multi_finemap.csv.gz"
     bool <- grepl(query, filelist)
     filelist <- filelist[bool]
-    print(paste(length(filelist),"files found matching query."))
+    printer(paste(length(filelist),"files found matching query."),v=verbose)
   }
   if(return_download_api){
     filelist <- file.path("https://github.com",creator,repo,"raw/master",filelist)
@@ -27,7 +102,9 @@ GITHUB.list_files <- function(creator="RajLabMSSM",
 GITHUB.download_files <- function(filelist,
                                    download_dir="./",
                                    overwrite=F,
-                                   nThread=parallel::detectCores()){
+                                   nThread=parallel::detectCores()-2,
+                                  verbose=T){
+  printer("+ Downloading",length(filelist),"files...",v=verbose)
   local_files <- parallel::mclapply(filelist, function(x){
     print(paste("Downloading",x))
     destfile <-  gsub("https://github.com/*.*/raw/master/www/data",
